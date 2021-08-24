a21d6417f92a5c98040e1da5f4af307b4f5893ef
cormenov <cormenov@uni.pe>
2021-08-23 22:38:58 -0500

    Copia Script
    Este archivo permitirá tener una copia del Script inicial

---
+++ DraftCodeCopia.R
@@ -0,0 +1,10 @@
+#Código de Prueba para Proyecto MiPrueba
+library (dplyr)
+'
+  Autor: Carlos Ormeño
+  Title: Primer Script Borrador
+'
+
+#Creando una función suma
+suma = function (x,y){
+  x + y}
